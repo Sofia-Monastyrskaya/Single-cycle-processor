737a5b8b8115b2243d9a8d6156af86948909e018
Sofia-Monastyrskaya <sofi-mon2000@yandex.ru>
2026-02-10 00:47:54 +0300

    add testbench

--- src/main/scala/Adder/FullAdder.scala
+++ src/main/scala/Adder/FullAdder.scala
@@ -1,6 +1,8 @@
 package Adder
 
 import spinal.core._
+import spinal.sim._
+import spinal.core.sim._
 
 //import scala.language.postfixOps
 
@@ -20,37 +22,42 @@ case class FullAdder() extends Component {
 
 }
 
-//object FullAdderSim extends App {
-//  SimConfig
-//    .compile(FullAdder())
-//    .doSim{ dut =>
-//
-//      dut.io.a_i     #= false
-//      dut.io.b_i     #= false
-//      dut.io.carry_i #= false
-//      dut.clockDomain.waitSampling()
-//      println(s"a=0 b=0 cin=0 -> sum=${dut.io.sum_o.toBoolean} carry=${dut.io.carry_o.toBoolean}")
-//
-//      dut.io.a_i     #= false
-//      dut.io.b_i     #= true
-//      dut.io.carry_i #= false
-//      dut.clockDomain.waitSampling()
-//      println(s"a=0 b=1 cin=0 -> sum=${dut.io.sum_o.toBoolean} carry=${dut.io.carry_o.toBoolean}")
-//
-//      dut.io.a_i     #= true
-//      dut.io.b_i     #= false
-//      dut.io.carry_i #= false
-//      dut.clockDomain.waitSampling()
-//      println(s"a=1 b=0 cin=0 -> sum=${dut.io.sum_o.toBoolean} carry=${dut.io.carry_o.toBoolean}")
-//
-//      dut.io.a_i     #= true
-//      dut.io.b_i     #= true
-//      dut.io.carry_i #= false
-//      dut.clockDomain.waitSampling()
-//      println(s"a=1 b=1 cin=0 -> sum=${dut.io.sum_o.toBoolean} carry=${dut.io.carry_o.toBoolean}")
-//
-//    }
-//}
+object FullAdderSim extends App {
+  SimConfig
+    .withVerilator
+    .compile(FullAdder())
+    .doSim{ dut =>
+
+      dut.io.a_i     #= false
+      dut.io.b_i     #= false
+      dut.io.carry_i #= false
+      //dut.clockDomain.waitSampling()
+      sleep(1)
+      println(s"a=0 b=0 cin=0 -> sum=${dut.io.sum_o.toBoolean} carry=${dut.io.carry_o.toBoolean}")
+
+      dut.io.a_i     #= false
+      dut.io.b_i     #= true
+      dut.io.carry_i #= false
+     // dut.clockDomain.waitSampling()
+      sleep(1)
+      println(s"a=0 b=1 cin=0 -> sum=${dut.io.sum_o.toBoolean} carry=${dut.io.carry_o.toBoolean}")
+
+      dut.io.a_i     #= true
+      dut.io.b_i     #= false
+      dut.io.carry_i #= false
+     // dut.clockDomain.waitSampling()
+      sleep(1)
+      println(s"a=1 b=0 cin=0 -> sum=${dut.io.sum_o.toBoolean} carry=${dut.io.carry_o.toBoolean}")
+
+      dut.io.a_i     #= true
+      dut.io.b_i     #= true
+      dut.io.carry_i #= false
+     // dut.clockDomain.waitSampling()
+      sleep(1)
+      println(s"a=1 b=1 cin=0 -> sum=${dut.io.sum_o.toBoolean} carry=${dut.io.carry_o.toBoolean}")
+
+    }
+}
 
 object FullAdderVerilog extends App {
   SpinalConfig(
